0c7081888332d19c8bb4c8b90f831b3dd3db611f
seleborg <seleborg@users.noreply.github.com>
2015-02-01 10:11:14 +0100

    Automatically compute total sum paid by each person

--- js/expensesCtrl.js
+++ js/expensesCtrl.js
@@ -10,4 +10,41 @@ vacationExpensesApp.controller('ExpensesCtrl', function ($scope) {
 		{person: "Joe", 	amount: "12", purpose: "Coffee"}
 	];
 
+	$scope.people = {};
+
+	$scope.$watch('expenses', function (value) {
+		$scope.people = recreatePeople(value);
+	}, true);
 });
+
+
+function recreatePeople(expenses) {
+	var people = {};
+
+	for(var i = 0; i < expenses.length; i++) {
+		var expense = expenses[i];
+		var name = expense.person;
+		var person = people[name] || {paid: [], name: name};
+		person.paid.push(Number(expense.amount));
+		people[name] = person;
+	}
+
+	return computeTotalPaidPerPerson(people);
+}
+
+
+function computeTotalPaidPerPerson(people) {
+	var newpeople = angular.copy(people);
+
+	for (name in newpeople) {
+		var person = newpeople[name];
+		var total = 0;
+		for (var i = 0; i < person.paid.length; i++) {
+			total += person.paid[i];
+		}
+
+		person.totalPaid = total;
+	}
+
+	return newpeople;
+}
