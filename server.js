46100847e3388341d5884f18de5e970456383b21
seleborg <seleborg@users.noreply.github.com>
2015-02-22 16:33:59 +0100

    Serve index.html as /, serve js/ and lib/ as static resources

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 console.log("Node.js version " + process.version);
 
 var express = require('express');
-var serveStatic = require('serve-static');
 
 var app = express();
 
-app.use(serveStatic('.', {index: "index.html"}));
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/lib', express.static(__dirname + '/lib'));
+
 app.listen(process.env.PORT || 3000);
 console.log("Startup completed, listening.");
