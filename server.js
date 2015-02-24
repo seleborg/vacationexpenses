6c98c017d42bad3f34a253f3b2e208710b7c6b65
seleborg <seleborg@users.noreply.github.com>
2015-02-24 23:23:39 +0100

    Add simple HTTP API to fetch a bill's JSON data
    This introduces a new Node module called 'storage', which implements
    a single function to fetch a bill's JSON data given the bill's publicly
    visible URL. The implementation currently only uses static test data,
    and does not actually fetch any data from any storage service.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
+var storage = require('./storage.js');
 
 var app = express();
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
+
+app.get('/bills/*', function (req, res) {
+	var url = req.params[0];
+	storage.fetchBill(url, function (response) {
+		res.status(response.status).json(response);
+	});
+});
+
 app.use('/js', express.static(__dirname + '/js'));
 app.use('/lib', express.static(__dirname + '/lib'));
 
---
+++ storage.js
@@ -0,0 +1,52 @@
+var azureStorage = require('azure-storage');
+
+var tableService = azureStorage.createTableService();
+
+var TABLE_NAME = 'bills';
+var PARTITION_KEY = 'bills';
+
+var testBills = {
+	'example1': {
+		status: 200,
+		version: 1,
+		bill: {
+			expenses: [
+				{name: "Joe", amount: "99", purpose: "Whisky",
+					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
+				{name: "Laura", amount: "12", purpose: "Cheese",
+					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
+				{name: "Laura", amount: "37", purpose: "Restaurant Saturday",
+					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
+				{name: "Joe", amount: "45", purpose: "Movie",
+					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
+				{name: "Joe", amount: "12", purpose: "Coffee",
+					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}}
+			]
+		}
+	}
+};
+
+
+// Fetch a bill given the bill's url.
+// callback = function (response)
+// where response is a structure of the format:
+// { status: One of the standard HTTP status codes, 200 if successful.
+//   url: the bill's URL (same as input value)
+//   version: the bill's data version (used to translate from older formats) (null unless status == 200).
+//   bill: the bill structure (null unless status == 200).
+// }
+exports.fetchBill = function (url, callback) {
+	if (url in testBills) {
+		var response = testBills[url];
+		response.url = url;
+		callback(response);
+	}
+	else {
+		callback({
+			status: 404,
+			url: url,
+			version: null,
+			bill: null,
+		});
+	}
+}
