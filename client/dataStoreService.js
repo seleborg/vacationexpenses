f354f88bc56692774107b41d881c2d108d820d8a
seleborg <seleborg@users.noreply.github.com>
2015-03-18 23:27:12 +0100

    Implement dataStore.storeDelayed()

--- client/dataStoreService.js
+++ client/dataStoreService.js
@@ -1,29 +1,130 @@
 angular.module('vacationExpenses.dataStore', [])
-	.factory('dataStore', ['$http', function ($http) {
+	.factory('dataStore', ['$http', '$timeout', '$q', function ($http, $timeout, $q) {
+		var delayInMillis = 1000;
+		var dataToStore;
+		var storeDefer;
+		var storePromise;
+		var runningTimeout;
+
+
+		/**
+		 * Wraps the given promise into an adpater promise that provides
+		 * the following two methods:
+		 *
+		 * success(fn): call fn(data, status) when the request succeeds.
+		 * error(fn): call fn(status) when the request fails.
+		 */
+		function adaptFetchPromise(promise) {
+			var adaptedPromise = Object.create(promise);
+
+			adaptedPromise.success = function (fn) {
+				promise.success(function (data, status, headers, options) {
+					fn(data, status);
+				});
+				return this;
+			};
+
+			adaptedPromise.error = function (fn) {
+				promise.error(function (data, status, headers, options) {
+					fn(status);
+				});
+				return this;
+			};
+
+			return adaptedPromise;
+		};
+
+
+		/**
+		 * Adapt the defer's promise to add success() and error() methods,
+		 * to mimic the promise returned by fetch().
+		 */
+		function adaptStorePromise(defer) {
+			defer.promise.success = function (fn) {
+				defer.promise.then(fn, null);
+				return this;
+			};
+			defer.promise.error = function (fn) {
+				defer.promise.then(null, fn);
+				return this;
+			};
+			return defer.promise;
+		}
+
+
+		/**
+		 * Actually send the data saved in the dataToStore member using $http
+		 * to the storage API.
+		 */
+		function storeDelayedData() {
+			cancelRunningTimeout();
+
+			$http.put('/api/v1/bills/' + dataToStore.billId, dataToStore.data)
+				.success(function (data, status, headers, options) {
+					storeDefer.resolve(status);
+					storeDefer = undefined;
+				})
+				.error(function (data, status, headers, options) {
+					storeDefer.reject(status);
+					storeDefer = undefined;
+				});
+		}
+
+
+		/**
+		 * Cancel the currently running $timeout.
+		 */
+		function cancelRunningTimeout() {
+			if (angular.isDefined(runningTimeout)) {
+				$timeout.cancel(runningTimeout);
+				runningTimeout = undefined;
+			}
+		}
+
+
+		/**
+		 * dataStore's public API.
+		 */
 		var dataStore = {
+			/**
+			 * Delay before storing in milliseconds.
+			 */
+			delayInMillis: delayInMillis,
+
+
+			/**
+			 * Fetch the bill data using the given bill identifier. The function
+			 * returns a promise with the following two specific methods:
+			 *
+			 * promise.success(fn): call fn(data, status) when the promise is resolved.
+			 * promise.error(fn): call fn(status) when the promise is resolved.
+			 */
 			fetch: function (billId) {
-				var promise = $http.get('/api/v1/bills/' + billId);
+				return adaptFetchPromise($http.get('/api/v1/bills/' + billId));
+			},
 
-				// We clone the object, and provide adapted success() and error()
-				// methods that hide some of the complexity of HTTP requests
-				// (e.g. the headers and config objects).
-				var adaptedPromise = Object.create(promise);
 
-				adaptedPromise.success = function (fn) {
-					promise.success(function (data, status, headers, options) {
-						fn(data, status);
-					});
-					return this;
+			/**
+			 * Stores a copy of the data, overwriting any previously stored
+			 * data, and sets a timer to send the data to the actual storage
+			 * eventually.
+			 */
+			storeDelayed: function (billId, data) {
+				dataToStore = {
+					billId: billId,
+					data: angular.copy(data)
 				};
 
-				adaptedPromise.error = function (fn) {
-					promise.error(function (data, status, headers, options) {
-						fn(status);
-					});
-					return this;
-				};
+				cancelRunningTimeout();
 
-				return adaptedPromise;
+				if (!angular.isDefined(storeDefer)) {
+					storeDefer = $q.defer();
+					storePromise = adaptStorePromise(storeDefer);
+				}
+
+				runningTimeout = $timeout(storeDelayedData, delayInMillis);
+
+				return storePromise;
 			}
 		};
 
--- client/expensesCtrl.js
+++ client/expensesCtrl.js
@@ -33,21 +33,18 @@ vacationExpensesApp.controller('ExpensesCtrl', [
 	$scope.bill = null;
 	$scope.result = null;
 
-	$scope.isSaving = false;
-
 	$scope.$watch('bill', function (bill) {
 		if (bill) {
 			$scope.result = recalculateResult(bill);
-
-			$scope.isSaving = true;
 
 			var data = {
 				version: 1,
 				bill: $scope.bill
 			};
-			$http.put('/api/v1/bills/' + $scope.url, data)
-				.success(function () { $scope.isSaving = false; })
-				.error(function () { $scope.isSaving = false; });
+			dataStore.storeDelayed($scope.url, data)
+				.error(function (status) {
+					$scope.error = { status: status };
+				});
 		}
 	}, true);
 
