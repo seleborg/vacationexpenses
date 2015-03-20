0650701a2ce2ad72a0e1bb3aa88e4ebf37181eff
seleborg <seleborg@users.noreply.github.com>
2015-03-20 15:18:45 +0100

    Style only: replace double quotes with single quotes.

--- server/storage.js
+++ server/storage.js
@@ -12,16 +12,16 @@ var testBills = {
 		version: 1,
 		bill: {
 			expenses: [
-				{name: "Joe", amount: "99", purpose: "Whisky",
-					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
-				{name: "Laura", amount: "12", purpose: "Cheese",
-					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
-				{name: "Laura", amount: "37", purpose: "Restaurant Saturday",
-					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
-				{name: "Joe", amount: "45", purpose: "Movie",
-					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}},
-				{name: "Joe", amount: "12", purpose: "Coffee",
-					sharingModel: {equalShares: true, shares: {"Joe": 1, "Laura": 1}}}
+				{name: 'Joe', amount: '99', purpose: 'Whisky',
+					sharingModel: {equalShares: true, shares: {'Joe': 1, 'Laura': 1}}},
+				{name: 'Laura', amount: '12', purpose: 'Cheese',
+					sharingModel: {equalShares: true, shares: {'Joe': 1, 'Laura': 1}}},
+				{name: 'Laura', amount: '37', purpose: 'Restaurant Saturday',
+					sharingModel: {equalShares: true, shares: {'Joe': 1, 'Laura': 1}}},
+				{name: 'Joe', amount: '45', purpose: 'Movie',
+					sharingModel: {equalShares: true, shares: {'Joe': 1, 'Laura': 1}}},
+				{name: 'Joe', amount: '12', purpose: 'Coffee',
+					sharingModel: {equalShares: true, shares: {'Joe': 1, 'Laura': 1}}}
 			]
 		}
 	}
