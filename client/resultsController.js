9267fb96be81e3e4b52f1827e699000b707f4a0e
seleborg <seleborg@users.noreply.github.com>
2015-03-25 20:39:38 +0100

    Add drop-down to select currency for each person
    The idea is that John, who lives in the US, may want to have his
    results in USD, whereas Laura, who lives in Europe, wants her results
    in EUR.
    
    Note that I'm still using angular's currency filter, which will display
    amounts prefixed with the dollar-sign.

--- client/resultsController.js
+++ client/resultsController.js
@@ -1,21 +1,35 @@
 angular.module('vacationExpenses.resultsController', [])
 	.controller('resultsController', ['$scope', function ($scope) {
+		$scope.preferredCurrency = {};
+		$scope.results = {};
+
 		$scope.$watch('billLoaded', function (loaded) {
 			if (loaded) {
-				$scope.bill.onUpdated($scope.onBillUpdated);
-				$scope.onBillUpdated();
+				$scope.bill.onUpdated($scope.updateResults);
+				$scope.updateResults();
 			}
 		});
-		$scope.onBillUpdated = function () {
+
+		$scope.$watch('preferredCurrency', function (newValue, oldValue) {
+			if (newValue !== oldValue) {
+				$scope.updateResults();
+			}
+		}, true);
+
+		$scope.updateResults = function () {
 			$scope.results = {};
 			angular.forEach($scope.bill.names, function (name) {
-				var paid = $scope.bill.calculatePaid(name, 'USD');
-				var due = $scope.bill.calculateDue(name, 'USD')
+				if (!angular.isDefined($scope.preferredCurrency[name])) {
+					$scope.preferredCurrency[name] = 'USD';
+				}
+
+				var paid = $scope.bill.calculatePaid(name, $scope.preferredCurrency[name]);
+				var due = $scope.bill.calculateDue(name, $scope.preferredCurrency[name])
 				$scope.results[name] = {
 					name: name,
 					totalPaid: paid,
 					totalDue: due,
-					balance: paid - due,
+					balance: paid - due
 				};
 			});
 		};
