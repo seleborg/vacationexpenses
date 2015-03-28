2d282b9fdafbdbafaf2c096fc6ab2e873e023e58
seleborg <seleborg@users.noreply.github.com>
2015-03-28 23:05:05 +0100

    expenseController.$watch('expense'): Avoid unnecessary updates

--- client/expenseController.js
+++ client/expenseController.js
@@ -2,7 +2,9 @@ angular.module('vacationExpenses.expenseController', [])
 	.controller('expenseController', ['$scope', function ($scope) {
 		$scope.expense = angular.copy($scope.expense);
 
-		$scope.$watch('expense', function (expense) {
-			$scope.bill.updateExpense($scope.$index, expense);
+		$scope.$watch('expense', function (expense, oldExpense) {
+			if (expense !== oldExpense) {
+				$scope.bill.updateExpense($scope.$index, expense);
+			}
 		}, true);
 	}]);
