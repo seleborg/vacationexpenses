764ecf2d619a3113b68e2b27af2d0803c669c84a
seleborg <seleborg@users.noreply.github.com>
2015-03-28 23:06:18 +0100

    Add some (commented-out) logging to make future debugging easier

--- client/expenseController.js
+++ client/expenseController.js
@@ -4,6 +4,7 @@ angular.module('vacationExpenses.expenseController', [])
 
 		$scope.$watch('expense', function (expense, oldExpense) {
 			if (expense !== oldExpense) {
+				// console.log('expenseController.$watch.expense');
 				$scope.bill.updateExpense($scope.$index, expense);
 			}
 		}, true);
