b82415b17b2facc09aa1aecfaad268c16d4dcfac
seleborg <seleborg@users.noreply.github.com>
2015-03-29 19:46:10 +0200

    Fix: Share input for new participant would be empty
    When adding an expense with a new participant, the input fields for their
    number of shares for all other expenses would be added, but would remain
    empty, even though the actual sharingModels were all updated correctly.
    
    The reason is that since the expenseController works with a copy of the
    expense object, it wouldn't automatically receive updates when an expense
    was changed from the outside, which is the case when shares are kept
    up to date with the expenses.
    
    Bug was introduced with 3648c362e1f3de, where using the original instance
    instead of a copy was forgotten.

--- client/expenseController.js
+++ client/expenseController.js
@@ -1,7 +1,5 @@
 angular.module('vacationExpenses.expenseController', [])
 	.controller('expenseController', ['$scope', function ($scope) {
-		$scope.expense = angular.copy($scope.expense);
-
 		$scope.$watch('expense', function (expense, oldExpense) {
 			if (expense !== oldExpense) {
 				// console.log('expenseController.$watch.expense');
