4316b2cf7e544b977db4a0793ee37c43f6831711
seleborg <seleborg@users.noreply.github.com>
2015-03-29 22:19:06 +0200

    Show shares as active/inacive participants (or input boxes if !equalShares)

--- client/expenseController.js
+++ client/expenseController.js
@@ -1,9 +1,23 @@
 angular.module('vacationExpenses.expenseController', [])
 	.controller('expenseController', ['$scope', function ($scope) {
+		$scope.participantStatus = {};
+
 		$scope.$watch('expense', function (expense, oldExpense) {
 			if (expense !== oldExpense) {
 				// console.log('expenseController.$watch.expense');
 				$scope.bill.updateExpense($scope.$index, expense);
 			}
+			$scope.participantStatus = {};
+			angular.forEach(expense.sharingModel.shares, function (share, name) {
+				if (expense.sharingModel.equalShares) {
+					$scope.participantStatus[name] =
+						share == 0
+						? 'participant-inactive'
+						: 'participant-active';
+				}
+				else {
+					$scope.participantStatus[name] = 'participant-number';
+				}
+			})
 		}, true);
 	}]);
