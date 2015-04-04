45b897e69af7e18e0bc68eb4ad16069b967156e4
seleborg <seleborg@users.noreply.github.com>
2015-04-04 23:27:44 +0200

    Create new bill when clicking the big blue button

--- client/homepageController.js
+++ client/homepageController.js
@@ -1,7 +1,26 @@
-angular.module('vacationExpenses.homepageController', [])
-	.controller('homepageController', ['$scope', function ($scope) {
+angular.module('vacationExpenses.homepageController', [
+	'vacationExpenses.billService'
+	])
+	.controller('homepageController', [
+		'$scope',
+		'$http',
+		'$window',
+		'billService', function ($scope, $http, $window, billService) {
+
 		$scope.createNewBill = function () {
-			console.log('Create new bill');
+			var billData = {
+				version: 1,
+				bill: billService.createBill(),
+			};
+
+			$http.post('/api/v1/bills', {'billData': billData})
+				.success(function (data, status, headers, options) {
+					var url = headers().location;
+					$window.location.href = url;
+				})
+				.error(function (data, status, headers, options) {
+
+				});
 		}
 	}])
 ;
