54d732f9b6cfbf04db8c83c15edba6a124d33e7d
Carl Seleborg <seleborg@users.noreply.github.com>
2015-10-25 17:52:02 +0100

    Use Firefox for selenium tests
    There seems to be a problem currently with Chrome 46 and protractor, and
    the tests currently don't run (the browser just hangs after startup and
    Selenium can't get it to load a URL or otherwise communicate with it.)
    Firefox works well, so we'll use that instead.

--- test/client/e2e/creatingBillSpec.js
+++ test/client/e2e/creatingBillSpec.js
@@ -1,7 +1,7 @@
 describe('vacationExpenses.creatingBill', function () {
 	describe('homepage', function () {
 		it('should have a "create bill" button', function () {
-			browser.get('http://localhost:3000/');
+			browser.get('/');
 			expect(element(by.id('createBillButton')).isPresent()).toBeTruthy();
 		});
 	});
--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,8 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
-	specs: ['client/e2e/*Spec.js']
+	specs: ['client/e2e/*Spec.js'],
+	baseUrl: 'http://localhost:3000',
+	capabilities: {
+		browserName: "firefox"
+	}
 }
