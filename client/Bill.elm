6b7e3263fe8e99116c4856594d42cd03a00c52e5
seleborg <seleborg@users.noreply.github.com>
2016-03-21 15:24:19 +0100

    Add an amount to an expense

--- client/Bill.elm
+++ client/Bill.elm
@@ -11,6 +11,7 @@ module Bill (Expense, Bill, addExpense) where
 -}
 type alias Expense =
   { name : String
+  , amount : Float
   }
 
 
--- client/BillPage.elm
+++ client/BillPage.elm
@@ -8,68 +8,84 @@ import Json.Decode
 import List
 import Maybe
 import StartApp.Simple
+import String
 
 import Bill
 
 
-type alias NewExpense = 
-  { name : String 
+type alias NewExpense =
+  { name : String
+  , amount : String
   }
 
 
-type alias Model = 
+type alias Model =
   { bill : Bill.Bill
   , newExpense : NewExpense
   }
 
 
-type Action = 
+type Action =
   UpdateNewExpenseName String
+  | UpdateNewExpenseAmount String
   | AddExpense
 
 
 init : Model
 init =
-  { bill = 
-    { expenses = 
-      [ { name = "John" }
-      , { name = "Jack" }
+  { bill =
+    { expenses =
+      [ { name = "John", amount = 50 }
+      , { name = "Jack", amount = 10 }
       ]
     }
-  , newExpense = 
+  , newExpense =
     { name = ""
+    , amount = ""
     }
   }
 
 
 viewExpense : Bill.Expense -> Html.Html
 viewExpense expense =
-  Html.li [] [ Html.text expense.name ]
+  Html.li
+    []
+    [ Html.text
+      (expense.name ++ " spent " ++ (Basics.toString expense.amount)) ]
 
 
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
   Html.div []
   [ Html.div [] [ Html.ol [] (List.map viewExpense model.bill.expenses) ]
-  , Html.form 
-    [ Html.Events.onWithOptions 
-        "submit" 
+  , Html.form
+    [ Html.Events.onWithOptions
+        "submit"
         { stopPropagation = False, preventDefault = True}
         (Json.Decode.succeed Maybe.Nothing)
         (\_ -> (Signal.message address AddExpense))
-    ] 
-    [ Html.input 
+    ]
+    [ Html.input
       [ Html.Attributes.value model.newExpense.name
-      , Html.Events.on 
-        "input" 
-        Html.Events.targetValue 
-        (\value -> (Signal.message address (UpdateNewExpenseName value))) 
+      , Html.Events.on
+        "input"
+        Html.Events.targetValue
+        (\value -> (Signal.message address (UpdateNewExpenseName value)))
+      ]
+      []
+    , Html.input [
+        Html.Attributes.value model.newExpense.amount
+      , Html.Events.on
+        "input"
+        Html.Events.targetValue
+        (\value -> (Signal.message address (UpdateNewExpenseAmount value)))
       ]
       []
     , Html.button [] [Html.text "Add"]]
   ]
 
 
+update : Action -> Model -> Model
 update action model =
   case action of
     UpdateNewExpenseName newName ->
@@ -78,11 +94,22 @@ update action model =
       in
         { model | newExpense = { newExpense | name = newName } }
 
+    UpdateNewExpenseAmount newAmount ->
+      let
+        newExpense = model.newExpense
+      in
+        { model | newExpense = { newExpense | amount = newAmount } }
+
     AddExpense ->
-      { model 
-        | bill = Bill.addExpense model.bill { name = model.newExpense.name }
-        , newExpense = { name = "" } 
-      }
+      let
+        name = model.newExpense.name
+        amount = Result.withDefault 0 (String.toFloat model.newExpense.amount)
+        newExpense = { name = name, amount = amount }
+      in
+        { model
+          | bill = (Bill.addExpense model.bill newExpense)
+          , newExpense = { name = "", amount = "" }
+        }
 
 
 main =
